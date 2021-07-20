34f97fcf2b89160ec212b14ee489e4e23712e613
Justin Wong <justincadburywong@gmail.com>
2021-07-20 16:21:05 -0700

    210719 add Resources#external#index and make searchable; i18n external#form

--- app/models/external.rb
+++ app/models/external.rb
@@ -1,4 +1,25 @@
 class External < ApplicationRecord
   extend FriendlyId
   friendly_id :en_title, use: :slugged
+  scope :filter_by_search, -> (search) { where("en_title like ?", "%#{search}%").or(
+                                         where("en_source like ?", "%#{search}%")).or(
+                                         where("en_content like ?", "%#{search}%")).or(
+                                         where("en_notes like ?", "%#{search}%")).or(
+                                         where("zh_tw_title like ?", "%#{search}%")).or(
+                                         where("zh_tw_source like ?", "%#{search}%")).or(
+                                         where("zh_tw_content like ?", "%#{search}%")).or(
+                                         where("zh_tw_notes like ?", "%#{search}%")).or(
+                                         where("zh_cn_title like ?", "%#{search}%")).or(
+                                         where("zh_cn_source like ?", "%#{search}%")).or(
+                                         where("zh_cn_content like ?", "%#{search}%")).or(
+                                         where("zh_cn_notes like ?", "%#{search}%")).or(
+                                         where("vi_title like ?", "%#{search}%")).or(
+                                         where("vi_source like ?", "%#{search}%")).or(
+                                         where("vi_content like ?", "%#{search}%")).or(
+                                         where("vi_notes like ?", "%#{search}%")).or(
+                                         where("hmn_title like ?", "%#{search}%")).or(
+                                         where("hmn_source like ?", "%#{search}%")).or(
+                                         where("hmn_content like ?", "%#{search}%")).or(
+                                         where("hmn_notes like ?", "%#{search}%"))
+                                       }
 end
