73c2c2f8ae592875657f2dc21a055b05e6061285
Justin Wong <justincadburywong@gmail.com>
2021-07-20 21:16:30 -0700

    210720 allow for FAQs and External searches to be case-insensitive

--- app/models/external.rb
+++ app/models/external.rb
@@ -1,25 +1,25 @@
 class External < ApplicationRecord
   extend FriendlyId
   friendly_id :en_title, use: :slugged
-  scope :filter_by_search, -> (search) { where("en_title like ?", "%#{search}%").or(
-                                         where("en_source like ?", "%#{search}%")).or(
-                                         where("en_content like ?", "%#{search}%")).or(
-                                         where("en_notes like ?", "%#{search}%")).or(
-                                         where("zh_tw_title like ?", "%#{search}%")).or(
-                                         where("zh_tw_source like ?", "%#{search}%")).or(
-                                         where("zh_tw_content like ?", "%#{search}%")).or(
-                                         where("zh_tw_notes like ?", "%#{search}%")).or(
-                                         where("zh_cn_title like ?", "%#{search}%")).or(
-                                         where("zh_cn_source like ?", "%#{search}%")).or(
-                                         where("zh_cn_content like ?", "%#{search}%")).or(
-                                         where("zh_cn_notes like ?", "%#{search}%")).or(
-                                         where("vi_title like ?", "%#{search}%")).or(
-                                         where("vi_source like ?", "%#{search}%")).or(
-                                         where("vi_content like ?", "%#{search}%")).or(
-                                         where("vi_notes like ?", "%#{search}%")).or(
-                                         where("hmn_title like ?", "%#{search}%")).or(
-                                         where("hmn_source like ?", "%#{search}%")).or(
-                                         where("hmn_content like ?", "%#{search}%")).or(
-                                         where("hmn_notes like ?", "%#{search}%"))
+  scope :filter_by_search, -> (search) { where("en_title ilike ?", "%#{search}%").or(
+                                         where("en_source ilike ?", "%#{search}%")).or(
+                                         where("en_content ilike ?", "%#{search}%")).or(
+                                         where("en_notes ilike ?", "%#{search}%")).or(
+                                         where("zh_tw_title ilike ?", "%#{search}%")).or(
+                                         where("zh_tw_source ilike ?", "%#{search}%")).or(
+                                         where("zh_tw_content ilike ?", "%#{search}%")).or(
+                                         where("zh_tw_notes ilike ?", "%#{search}%")).or(
+                                         where("zh_cn_title ilike ?", "%#{search}%")).or(
+                                         where("zh_cn_source ilike ?", "%#{search}%")).or(
+                                         where("zh_cn_content ilike ?", "%#{search}%")).or(
+                                         where("zh_cn_notes ilike ?", "%#{search}%")).or(
+                                         where("vi_title ilike ?", "%#{search}%")).or(
+                                         where("vi_source ilike ?", "%#{search}%")).or(
+                                         where("vi_content ilike ?", "%#{search}%")).or(
+                                         where("vi_notes ilike ?", "%#{search}%")).or(
+                                         where("hmn_title ilike ?", "%#{search}%")).or(
+                                         where("hmn_source ilike ?", "%#{search}%")).or(
+                                         where("hmn_content ilike ?", "%#{search}%")).or(
+                                         where("hmn_notes ilike ?", "%#{search}%"))
                                        }
 end
--- app/models/faq.rb
+++ app/models/faq.rb
@@ -7,10 +7,10 @@ class Faq < ApplicationRecord
   extend FriendlyId
   friendly_id :en_question, use: :slugged
   scope :filter_by_category, -> (category) { where category: category }
-  scope :filter_by_search, -> (search) { where("en_question like ?", "%#{search}%").or(
-                                         where("zh_tw_question like ?", "%#{search}%")).or(
-                                         where("zh_cn_question like ?", "%#{search}%")).or(
-                                         where("vi_question like ?", "%#{search}%")).or(
-                                         where("hmn_question like ?", "%#{search}%"))
+  scope :filter_by_search, -> (search) { where("en_question ilike ?", "%#{search}%").or(
+                                         where("zh_tw_question ilike ?", "%#{search}%")).or(
+                                         where("zh_cn_question ilike ?", "%#{search}%")).or(
+                                         where("vi_question ilike ?", "%#{search}%")).or(
+                                         where("hmn_question ilike ?", "%#{search}%"))
                                        }
 end
