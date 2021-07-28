e4679bd4895f1345fbbd0bdeddd197830505fb7a
Justin Wong <justincadburywong@gmail.com>
2021-07-27 23:58:49 -0700

    210727 remove nested array for profile reorder

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,7 @@ class ProfilesController < ApplicationController
 
   # GET /profiles or /profiles.json
   def index
-    @profiles = Profile.all
+    @profiles = Profile.all.order('lastname ASC')
   end
 
   # GET /profiles/1 or /profiles/1.json
