90e43ed79531543ea358f35eb3c4a666ffa03c92
Justin Wong <justincadburywong@gmail.com>
2022-01-10 23:27:09 -0800

    220110 configure audit logs to use request.remote_ip and ignore Cloudflare proxy IPs

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,35 @@ module Informed
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # set request.remote_ip proxy IPs for Cloudflare
+    check_spoofing = true
+    proxies = ["173.245.48.0/20",
+"103.21.244.0/22",
+"103.22.200.0/22",
+"103.31.4.0/22",
+"141.101.64.0/18",
+"108.162.192.0/18",
+"190.93.240.0/20",
+"188.114.96.0/20",
+"197.234.240.0/22",
+"198.41.128.0/17",
+"162.158.0.0/15",
+"104.16.0.0/13",
+"104.24.0.0/14",
+"172.64.0.0/13",
+"131.0.72.0/22",
+"2400:cb00::/32",
+"2606:4700::/32",
+"2803:f800::/32",
+"2405:b500::/32",
+"2405:8100::/32",
+"2a06:98c0::/29",
+"2c0f:f248::/32"]
+    proxies += ActionDispatch::RemoteIp::TRUSTED_PROXIES
+    config.middleware.swap ActionDispatch::RemoteIp,
+                           ActionDispatch::RemoteIp,
+                           true,
+                           proxies
   end
 end
