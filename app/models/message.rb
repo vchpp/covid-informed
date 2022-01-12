27a187794d5cc47eb983f155f94b3330b1f1a5ab
Justin Wong <justincadburywong@gmail.com>
2022-01-11 20:17:36 -0800

    220110 refactor Messages step 1

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many_attached :images, dependent: :destroy
+  has_many_attached :en_images, dependent: :destroy
   has_many_attached :vi_images, dependent: :destroy
   has_many_attached :zh_cn_images, dependent: :destroy
   has_many_attached :zh_tw_images, dependent: :destroy
