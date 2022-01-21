a82ebed08f088fb05905d272a424ac164023769f
Justin Wong <justincadburywong@gmail.com>
2022-01-21 14:19:26 -0800

    220121-healthwise implement oauth token retrieval and cache storage for API calls

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'jquery-rails'
 gem 'popper_js', '~> 1.12.9'
 gem "aws-sdk-s3", require: false
 gem 'dotenv-rails'
-# gem 'rails_12factor'
+gem 'rest-client'
 gem 'friendly_id', '~> 5.4.0'
 gem "activestorage-office-previewer"
 gem 'metamagic'
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,24 @@
 class AdminController < ApplicationController
   def index
+    token = fetch_token
+    logger.warn("HEALTHWISE API TOKEN IS #{token}")
   end
+  
+  private
+    require "rest-client"
+
+    def fetch_token
+    Rails.cache.fetch("/healthwise_api_token", expires_in: 1.hour) do
+      url = ENV['HEALTHWISE_AUTH_URL'] + "/oauth2/token"
+      response = RestClient::Request.execute(
+        method: :post,
+        url: url,
+        user: ENV['HEALTHWISE_CLIENT_ID'],
+        password: ENV['HEALTHWISE_CLIENT_SECRET'],
+        payload: "grant_type=client_credentials&scope=*"
+      )
+      JSON.parse(response.body)["access_token"]
+    end
+  end
+
 end
