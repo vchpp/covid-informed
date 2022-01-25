24f871c9debad0d6eccf36e23842a61d68e27819
Justin Wong <justincadburywong@gmail.com>
2022-01-24 23:02:05 -0800

    220121-healthwise cache json for POC

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,11 +6,17 @@ class AdminController < ApplicationController
 
   def healthwise
     # move all this logic to the healthwise/application controller
-    token = fetch_healthwise_token
-    logger.warn("HEALTHWISE AUTH TOKEN IS #{token}")
-    url = ENV['HEALTHWISE_CONTENT_URL'] + "/articles/ack8827/en-us"
-    @response = RestClient.get url, { "Authorization": "Bearer #{token}", "X-HW-Version": "1", "Accept": "application/json"}
-    logger.warn("#{@response}")
+    @response = fetch_article
+    logger.warn("#{Rails.cache.fetch("/hwid")}")
+  end
+
+private
+  def fetch_article
+    Rails.cache.fetch("/hwid", expires_in: 1.day) do
+      token = fetch_healthwise_token
+      url = ENV['HEALTHWISE_CONTENT_URL'] + "/articles/ack8827/en-us"
+      response = RestClient.get url, { "Authorization": "Bearer #{token}", "X-HW-Version": "1", "Accept": "application/json"}
+    end
   end
 
 end
