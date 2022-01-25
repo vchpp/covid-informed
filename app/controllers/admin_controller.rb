188bb7380999091c7ec86627dca492d27a7fbbc1
Justin Wong <justincadburywong@gmail.com>
2022-01-25 15:52:15 -0800

    220121-healthwise correctly display article POC

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,20 +1,31 @@
 class AdminController < ApplicationController
-  before_action :authenticate_admin!, only: %i[ healthwise ]
+  before_action :authenticate_admin!, only: %i[ healthwise_article healthwise_topic ]
 
   def index
   end
 
-  def healthwise
+  def healthwise_article
     # move all this logic to the healthwise/application controller
     @response = fetch_article
-    logger.warn("#{Rails.cache.fetch("/hwid")}")
+  end
+
+  def healthwise_topic
+    @response = fetch_topic
   end
 
 private
   def fetch_article
-    Rails.cache.fetch("/hwid", expires_in: 1.day) do
+    Rails.cache.fetch("/hwid-article", expires_in: 1.day) do
       token = fetch_healthwise_token
       url = ENV['HEALTHWISE_CONTENT_URL'] + "/articles/ack8827/en-us"
+      response = RestClient.get url, { "Authorization": "Bearer #{token}", "X-HW-Version": "1", "Accept": "application/json"}
+    end
+  end
+
+  def fetch_topic
+    Rails.cache.fetch("/hwid-topic-json", expires_in: 1.minute) do
+      token = fetch_healthwise_token
+      url = ENV['HEALTHWISE_CONTENT_URL'] + "/topics/ack9671/en-us?contentOutput=json"
       response = RestClient.get url, { "Authorization": "Bearer #{token}", "X-HW-Version": "1", "Accept": "application/json"}
     end
   end
