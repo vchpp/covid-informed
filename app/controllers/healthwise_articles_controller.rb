2e74b1e47284bd98727d2cafc48873b602995edb
Justin Wong <justincadburywong@gmail.com>
2022-02-01 21:38:07 -0800

    220131 getting somewhere with healthwise controllers

--- app/controllers/healthwise_articles_controller.rb
+++ app/controllers/healthwise_articles_controller.rb
@@ -9,17 +9,18 @@ class HealthwiseArticlesController < ApplicationController
   # GET /healthwise_articles/1 or /healthwise_articles/1.json
   def show
     # check if it's custom JSON, if yes, skip fetching
-    if @healthwise_article.send("#{I18n.locale}_translated") == false
+    if @healthwise_article.send("#{I18n.locale}_translated".downcase) == false
       # check if the HW JSON is out of date, then fetch_article:&update!
       if @healthwise_article.updated_at < Time.now - 7.days
         if @healthwise_article.type == "article"
-          @healthwise_article.send("#{I18n.locale}_json") = fetch_article(@healthwise_article)
+          @healthwise_article.send("#{I18n.locale}_json".downcase= fetch_article(@healthwise_article.hwid, HW_LOCALE[I18n.locale]))
         else
-          @healthwise_article.send("#{I18n.locale}_json") = fetch_topic(@healthwise_article)
+          @healthwise_article.send("#{I18n.locale}_json".downcase= fetch_topic(@healthwise_article.hwid, HW_LOCALE[I18n.locale]))
         end
+        @healthwise_article.update!
       end
-      @healthwise_article.update!
     end
+
   end
 
   # GET /healthwise_articles/new
@@ -39,19 +40,16 @@ class HealthwiseArticlesController < ApplicationController
     if @healthwise_article.type == "Article"
       # fetch article for available languages
       # store them in @healthwise_article.languages
-      @healthwise_article.languages = @fetch_article_languages(@healthwise_article)
+      @healthwise_article.languages = @fetch_article_languages(@healthwise_article.hwid)
       # fetch article's JSON from hwid for [languages], otherwise default to english
       @healthwise_article.languages.each do |l|   # ["en-us", "vi-us"]
         response = fetch_article(@healthwise_article.hwid, l)
-        @healthwise_article.send("#{set_ci_locale(l)}_json") = response
-        @healthwise_article.send("#{set_ci_locale(l)}_title") = response["data"]["title"]["consumer"]
+        @healthwise_article.send("#{CI_LOCALE[l]}_json") = response
+        @healthwise_article.send("#{CI_LOCALE[l]}_title") = response["data"]["title"]["consumer"]
       end
-
     else
       # fetch topic for available languages
-
     end
-
 
     # save
     respond_to do |format|
@@ -89,29 +87,32 @@ class HealthwiseArticlesController < ApplicationController
 
   private
 
-    def set_ci_locale(hw_lang)
-      case
-      when hw_lang == "vi-us"
-        return "vi"
-      when hw_lang == "hm-us"
-        return "hmn"
-      when hw_lang == "zh-us"
-        return "zh_cn"
-      when hw_lang == "en-us"
-        return "en"
-      end
-    end
+
+    CI_LOCALE = {
+      "vi-us"  => "vi",
+      "hm-us" => "hmn",
+      "zh-us" => "zh_cn",
+      "en-us" => "en"
+    }
+
+    HW_LOCALE = {
+      "vi"  => "vi-us",
+      "hmn" => "hm-us",
+      "zh-tw" => "zh-us",
+      "zh-cn" => "zh-us",
+      "en" => "en-us"
+    }
 
     LOCALES = Regexp.union(
       /en-us/i,
       /zh-us/i,
       /vi-us/i,
-      /hm-us/i
+      /hm-us/i,
     )
 
-    def fetch_article_languages(article)
+    def fetch_article_languages(hwid)
       token = fetch_hw_token
-      url = ENV['HEALTHWISE_CONTENT_URL'] + "/articles/#{article.hwid}"
+      url = ENV['HEALTHWISE_CONTENT_URL'] + "/articles/#{hwid}"
       response = RestClient.get url, { "Authorization": "Bearer #{token}", "X-HW-Version": "1", "Accept": "application/json"}
       # iterate over json hash to match for available locales #
       languages = []
@@ -131,7 +132,6 @@ class HealthwiseArticlesController < ApplicationController
       token = fetch_hw_token
       url = ENV['HEALTHWISE_CONTENT_URL'] + "/topics/#{topic.hwid}"
       response = RestClient.get url, { "Authorization": "Bearer #{token}", "X-HW-Version": "1", "Accept": "application/json"}
-
     end
 
     def fetch_topic(topic, language)
