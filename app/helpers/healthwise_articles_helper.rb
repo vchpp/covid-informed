8961b62008ef7bec5012c105a557ecd22757719b
Justin Wong <justincadburywong@gmail.com>
2022-02-03 20:46:25 -0800

    220131 clean up notices and alerts; add meta and gtag to healthwise pages

--- app/helpers/healthwise_articles_helper.rb
+++ app/helpers/healthwise_articles_helper.rb
@@ -2,9 +2,9 @@ module HealthwiseArticlesHelper
 
   def article_locale
     if @healthwise_article.send("#{params[:locale]}_json".downcase).present?
-      params[:locale]
+      params[:locale].downcase
     else
-      flash[:notice] = "Unfortunately this language is not available at this time.  Please check back at another time."
+      flash[:alert] = "Unfortunately this language is not available at this time.  Please check back at another time."
       "en"
     end
   end
